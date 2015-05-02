7a30b44c835c8da1bf9918881fcfdb1fc52ba36d
Nate Mara <natemara@gmail.com>
2015-05-02 19:01:10 -0400

    update readme and classifiers in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+from os import path
+
+BASE_PATH = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(BASE_PATH, 'README.rst'), encoding='utf-8') as f:
+	long_description = f.read()
 
 setup(
 	name='jumprunpro-python',
@@ -6,6 +13,7 @@ setup(
 	author='Nate Mara',
 	author_email='natemara@gmail.com',
 	description='A simple API for getting winds aloft data from NOAA',
+	long_description=long_description,
 	license='MIT',
 	test_suite='tests',
 	keywords='skydiving manifest',
@@ -15,6 +23,12 @@ setup(
 		'Development Status :: 3 - Alpha',
 		'Topic :: Utilities',
 		'License :: OSI Approved :: MIT License',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
 	],
 	install_requires=[
 		'beautifulsoup4==4.3.2',
