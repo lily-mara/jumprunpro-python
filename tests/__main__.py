c68fd4ed9ede9be88e8586714cf6f1ac9a4d0c10
Nate Mara <natemara@gmail.com>
2015-05-02 18:29:27 -0400

    remove unneeded code from tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,10 +1,4 @@
 from unittest import main
-from pprint import pprint
-
-from jumprun import JumprunProApi
-
 
 if __name__ == '__main__':
-	jump = JumprunProApi('skydive-warren-county')
-	pprint(jump.departing_loads())
 	main('tests')
